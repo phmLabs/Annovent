b2349a3c9aa190aec155314e39c5504d56e045e0
Baachi <info@bachi.biz>
2014-03-25 18:24:36 +0100

    Move phmLabs folder into src and use composer

---
+++ src/phmLabs/Components/Annovent/Annotation/Event.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace phmLabs\Components\Annovent\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * @Annotation
+ */
+class Event extends Annotation
+{
+  public $value;
+
+  public function getNames()
+  {
+    return (array)$this->value;
+  }
+}
